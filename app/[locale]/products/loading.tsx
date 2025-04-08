50f5753d15f70edb78decec9fc102e7622a06978
lucasbodev <lucasbodet.dev@gmail.com>
2025-04-08 14:46:22 +0200

    deleted products pages to success build

--- app/[locale]/products/loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ProductsLoading = () => {
-    return (
-        <span className="loading loading-ring loading-lg"></span>
-    );
-};
-
-export default ProductsLoading;
