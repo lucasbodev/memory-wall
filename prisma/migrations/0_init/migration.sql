195f896510ce180a887b0fe7767466482a6c5fcc
lucasbodev <lucasbodet.dev@gmail.com>
2025-04-08 14:42:34 +0200

    Test init db migration

--- prisma/migrations/0_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "count" (
-    "id" SERIAL NOT NULL,
-    "value" INTEGER,
-
-    CONSTRAINT "count_pkey" PRIMARY KEY ("id")
-);
-
--- prisma/migrations/20240324012317_pictures/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "player" ADD COLUMN     "pictureUrl" TEXT;
