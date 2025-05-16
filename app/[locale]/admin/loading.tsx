5559fa10502838c78fb12b4a16b95eab56bd4d4f
lucasbodev <lucasbodet.dev@gmail.com>
2025-05-16 17:05:06 +0200

    fixed loading style

--- app/[locale]/admin/loading.tsx
+++ app/[locale]/admin/loading.tsx
@@ -1,9 +1,13 @@
 import Loading from '@/components/loading/loading.component';
 import React from 'react';
+import styles from "@/app/[locale]/soldiers/soldiers.module.css"
 
 const AdminLoading = () => {
     return (
-        <Loading />
-)};
+        <div className={styles.adminPanel}>
+            <Loading />
+        </div>
+    )
+};
 
 export default AdminLoading;
